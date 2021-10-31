b2b4f251e634880d20ae6a5f8821984ce28f787a
Osmar da Cunha Filho <Osmar da Cunha Filho>
2021-10-31 17:11:07 -0300

    sugestoes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,20 +9,21 @@
     <ion-content>
       <div class='container'>
           <h1>Coordenadas atuais</h1>
-          <p>Latitude: {{  latitude  }}</p>
-          <p>Longitude: {{  longitude  }}</p>
+          <p>Latitude: {{ latitude }}</p>
+          <p>Longitude: {{ longitude }}</p>
           <ion-button expand="block" @click="buscaCidade()">REGISTRAR POSIÇÃO</ion-button>
       </div>
       <div>
-      <ion-grid>
-        <ion-row>
-          <ion-col>
-            <ul v-for="(cidade, index) in list" v-bind:key="index">
-              <li>{{cidade}}</li>
-            </ul>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
+        <ion-grid v-for="(registro, index) in list" v-bind:key="index">
+          <ion-row>
+            <ion-col>Cidade:</ion-col>
+            <ion-col>{{ registro.cidade }}</ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>Coordenadas:</ion-col>
+            <ion-col>{{ registro.coordenadas }}</ion-col>
+          </ion-row>
+        </ion-grid>
       </div>
     </ion-content>
   </ion-page>
@@ -71,8 +72,9 @@ export default defineComponent({
       if (JSON.parse(list) == null) {
         this.list = [];
       }
-      else
+      else {
         this.list = JSON.parse(list);
+      }
   },   
 
   methods: {
@@ -80,8 +82,8 @@ export default defineComponent({
       const coordinates = await Geolocation.getCurrentPosition();
       console.log('Current position: ', coordinates);
 
-        this.latitude = coordinates.coords.latitude;
-        this.longitude = coordinates.coords.longitude; 
+      this.latitude = coordinates.coords.latitude;
+      this.longitude = coordinates.coords.longitude;
     },
     buscaCidade: async function() {
         const ACCESS_KEY = '8cf68e16c982b92f6b8a0793785c61be';
@@ -93,22 +95,21 @@ export default defineComponent({
         const response = await Http.get(options);
         console.log('Resposta recebida: ', response);
 
-        this.cidade = response.data.data[0].locality + ',' + response.data.data[0].region_code;
-          this.coordenadas = this.latitude + ' , ' + this.longitude;
-          this.list.push("===========================");
-          this.list.push("Localidade: " + this.cidade);
-          this.list.push("Coordenadas: " + this.coordenadas);
-          await this.localStorage.set('list', JSON.stringify(this.list));
-          console.log(this.list);
-          const mensagem = this.cidade;
-          const alerta = await alertController
-           .create({
-          cssClass: 'my-custom-class',
-          header: 'Posição registrada!',
-          message: mensagem,
-          buttons: ['OK'],
-        });
-          await alerta.present();
+        this.cidade = response.data.data[0].locality + ', ' + response.data.data[0].region_code;
+        this.coordenadas = this.latitude + ' , ' + this.longitude;
+        this.list.push( { cidade: this.cidade, coordenadas: this.coordenadas } );
+        
+        await this.localStorage.set('list', JSON.stringify(this.list));
+        
+        console.log(this.list);
+        const mensagem = this.cidade;
+        const alerta = await alertController
+          .create({
+            header: 'Posição registrada!',
+            message: mensagem,
+            buttons: ['OK'],
+          });
+        await alerta.present();
     },
   }
 });
